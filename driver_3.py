5fc7dd62359279dd9804520aa9c27b181ebf3932
Khaliun Davaanyam <dhaliun0318@gmail.com>
2017-10-23 14:29:38 +0200

    second draft

--- driver_3.py
+++ driver_3.py
@@ -4,6 +4,7 @@ Created on Sat Oct 21 19:37:02 2017
 
 @author: Khaliun
 """
+import numpy
 class MoveNotPossible(Exception):
 
     
@@ -11,15 +12,9 @@ class MoveNotPossible(Exception):
         return "ulala"
     
 class state(object):
-    def __init__(self, l, label=None, parent=None):
+    def __init__(self, l, label, parent):
         
-        if len(l)==3:
-            self.data = l
-        else:
-            z0 = l[:3]
-            z1 = l[3:6]
-            z2 = l[6:]
-            self.data = [z0,z1,z2]           
+        self.data = list(l)
         self.parent = parent
         self.label = label
     def __str__(self):
@@ -51,64 +46,62 @@ class state(object):
         return self.data
         
     def move(self, move):
-        
-        copy = list(self.get_data())                     
+        copy =list(self.get_data())                            
         print(copy)
         print(copy is self.data)
         
-        for a in range(3):
-            for b in range(3):
-                if copy[a][b] == 0 :
+        for a in range(len(copy)):
+            if copy[a] == 0 :
                     i = a
-                    j = b
-                    
-                
+                                 
         
         print('data', self.data)
         
         if move == 'u': 
             
-            if i == 0:
+            if i < 3:
                 raise MoveNotPossible
             else:
-                temp = copy[i-1][j]
-                copy[i-1][j] = copy[i][j]
-                copy[i][j] = temp
-                lbl = 'Up'
-                return copy, lbl
+                temp = copy[i-2*3]
+                copy[i-2*3] = copy[i]
+                copy[i] = temp
+            lbl = 'Up'
+            return copy, lbl
+
         if move == 'd':
-            if i == 2:
+            if i > 5:
                 raise MoveNotPossible
             else:
-                temp = copy[i+1][j]
-                copy[i+1][j] = copy[i][j]
-                copy[i][j] = temp
-                lbl = 'Down'
-                return copy, lbl
-        if move == 'l':
-            
-            if j == 0:
+                temp = copy[i+3]
+                copy[i+3] = copy[i]
+                copy[i] = temp
+            lbl = 'Down'
+            return copy, lbl
+
+        if move == 'l':            
+            if i in [0,3,6]:
                 raise MoveNotPossible
             else:
-                temp = copy[i][j-1]
-                copy[i][j-1] = copy[i][j]
-                copy[i][j] = temp
-                lbl = 'Left'
-                return copy, lbl
+                temp = copy[i-1]
+                copy[i-1] = copy[i]
+                copy[i] = temp
+            lbl = 'Left'
+            return copy, lbl
+
         if move == 'r':
             
-            if j == 2:
+            if i in [2,5,8]:
                 raise MoveNotPossible
             else:
-                temp = copy[i][j+1]
-                copy[i][j+1] = copy[i][j]
-                copy[i][j] = temp
-                lbl = 'Right'
-                return copy, lbl            
+                temp = copy[i+1]
+                copy[i+1] = copy[i]
+                copy[i] = temp
+            lbl = 'Right'
+            return copy, lbl            
                     
                             
-    def is_goal(self, goal=[[0,1,2],[3,4,5],[6,7,8]]):
-        if self == goal :
+    def is_goal(self, goal=[0,1,2,3,4,5,6,7,8]):
+        if self.get_data() == goal :
             return True
         else:
             return False
@@ -122,47 +115,52 @@ class state(object):
             try :
                 data, label = self.move(char)
                 print('pp', data, label)
-                child_state = state(data,label)
-                print('child', child_state is self)
+                child_state = state(data,label,parent = self)
                 children.append(child_state)
             except MoveNotPossible:
                 pass
-                       
+        print(children, 'children')               
         return children
         
 def solve_bfs(first_state):
     
     to_visit = []
     visited = []
-    current_state = state(first_state)
+    current_state = state(first_state, 'first', 'NoParent' )
     to_visit.append(current_state)
     
     while True:
         
-        children = current_state.generate_children()
-                    
-        for i in children:
-            if i not in visited:
-                to_visit.append(i)
-        visited.append(current_state)
-        to_visit.remove(current_state)
-        current_state = to_visit[0]
-
         if current_state.is_goal() == True :
-            break
+
+            visited.append(current_state)
+            to_visit.remove(current_state)
+            break            
+        else:
+            children = current_state.generate_children()
+                    
+            for i in children:
+                if i.get_data() not in visited:
+                    to_visit.append(i)
+            visited.append(current_state)
+            to_visit.remove(current_state)
+            print(i.get_data() in to_visit)
+            current_state = to_visit[0]
 
     path_reversed = []
-    while current_state != None:
-        path_reversed.append(current_state)
+    while current_state.get_parent() != 'NoParent':
+        path_reversed.append(current_state.get_label())
         current_state = current_state.get_parent()
-        
-    path = path_reversed.reverse()
+        print(path_reversed, 'path_reversed ')
+    print(path_reversed.reverse(), 'path_reversed after')    
     
-    return path
     
-l = [1,0,2,3,4,5,6,7,8]
+    
+    return path_reversed.reverse()
+    
+l = [1,2,0,3,4,5,6,7,8]
 
-print(solve_bfs(l))
+print(solve_bfs(l), 'path')
         
             
             
