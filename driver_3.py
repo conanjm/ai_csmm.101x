51dfc1a87a5008ecdac4b68dd199faed9d4ce69b
Khaliun Davaanyam <dhaliun0318@gmail.com>
2017-11-02 12:41:34 +0100

    solve function, output.txt implemented

--- driver_3.py
+++ driver_3.py
@@ -5,6 +5,10 @@ Created on Sat Oct 21 19:37:02 2017
 @author: Khaliun
 """
 import numpy
+import sys
+import time
+import resource
+
 class MoveNotPossible(Exception):
 
     
@@ -12,11 +16,12 @@ class MoveNotPossible(Exception):
         return "ulala"
     
 class state(object):
-    def __init__(self, l, label, parent):
+    def __init__(self,depth, l, label, parent = 'NoParent'):
         
         self.data = list(l)
         self.parent = parent
         self.label = label
+        self.depth = depth
     def __str__(self):
         
         return self.data
@@ -47,15 +52,15 @@ class state(object):
         
     def move(self, move):
         copy =list(self.get_data())                            
-        print(copy)
-        print(copy is self.data)
+        #print(copy)
+        #print(copy is self.data)
         
         for a in range(len(copy)):
             if copy[a] == 0 :
                     i = a
                                  
         
-        print('data', self.data)
+        #print('data', self.data)
         
         if move == 'u': 
             
@@ -111,89 +116,118 @@ class state(object):
         children = []
         
         for char in 'udlr':
-            print(char)
+            #print(char)
             try :
                 data, label = self.move(char)
-                print('pp', data, label)
-                child_state = state(data,label,parent = self)
+                #print('pp', data, label)
+                child_state = state((self.depth+1),data,label,parent = self)
                 children.append(child_state)
             except MoveNotPossible:
                 pass
-        print(children, 'children')               
+        #print(children, 'children')               
         return children
-        
-def solve_bfs(first_state):
 
-    """ Breadth First Search """
-    
-    to_visit = []
-    visited = set()
-    current_state = state(first_state, 'first', 'NoParent' )
-    to_visit.append(current_state)
-    
-    while True:
+class solver(object):
+    def __init__(self, start_list, method):
         
-        if current_state.is_goal() == True :
+        self.start = state(0,start_list, 'first')
+        self.method = method
+        self.visited =  set()
+        self.all_state_set = set()
+        self.to_visit = []
+        self.max_search_depth = 0
+        self.running_time = 0   #the total running time of the search instance, reported in seconds
+        self.max_ram_usage = 0
 
-            visited.add(current_state)
-            to_visit.remove(current_state)
-            break            
-        else:
-            children = current_state.generate_children()
+           
+    def solve_bfs(self):
+
+        """ Breadth First Search """
+        current_state = self.start
+        self.all_state_set.add(str(current_state.data))       
+        while True:
+            self.visited.add(str(current_state.data))
+            if current_state.is_goal() == True :  
+                break            
+            else:
+                
+                children = current_state.generate_children()
+                     
+                for i in children:                    
+                    if str(i.data) not in self.all_state_set:
+                        self.to_visit.append(i)
+                        self.all_state_set.add(str(i.data))
+                        self.max_search_depth = max(self.max_search_depth, i.depth)
+
+                        
                     
-            for i in children:
-                if (i not in visited) and (i not in to_visit):
-                    to_visit.append(i)
-            visited.add(current_state)
-            to_visit.remove(current_state)
-            current_state = to_visit[0]
-
-    path = []
-    while current_state.get_parent() != 'NoParent':
-        path.append(current_state.get_label())
-        current_state = current_state.get_parent()
-               
-    path.reverse()   
-    return path
-
-def solve_dfs(first_state):
-    """Depth First Search"""
-
-    to_visit = []
-    visited = set()
-    current_state = state(first_state, 'first', 'NoParent' )
-    to_visit.append(current_state)
-    
-    while True:
+                current_state = self.to_visit.pop(0)
+        return current_state
         
-        if current_state.is_goal() == True :
+    def solve_dfs(self):
+        """Depth First Search"""
+        current_state = self.start
+        self.all_state_set.add(str(current_state.data))
+        while True:
+            if not len(self.visited)%1000:
+                print (len(self.visited))
+            self.visited.add(str(current_state.data))
+                        
+            if current_state.is_goal() == True :  
+                
 
-            visited.add(current_state)
-            to_visit.remove(current_state)
-            break            
-        else:
-            children = current_state.generate_children()
-            children.reverse()
+                break            
+            else:
+                children = current_state.generate_children()
+                children.reverse()
+                 
+                for i in children:
                     
-            for i in children:
-                if (i not in visited) and (i not in to_visit):
-                    to_visit.append(i)
-            visited.add(current_state)
-            to_visit.remove(current_state)
-            current_state = to_visit[-1]
+                    if str(i.data) not in self.all_state_set:
+                        self.to_visit.append(i)
+                        self.all_state_set.add(str(i.data))
+                        self.max_search_depth = max(self.max_search_depth, i.depth)
+                                        
+                current_state = self.to_visit.pop()
+        return current_state
+        
+    def find_path(self, c_state):
+        """ finds the path from the start state to the given state."""
+        path = []
+        while c_state.get_parent() != 'NoParent':
+            path.append(c_state.get_label())
+            c_state = c_state.get_parent()
+                
+        path.reverse()   
+        return path
 
-    path = []
-    while current_state.get_parent() != 'NoParent':
-        path.append(current_state.get_label())
-        current_state = current_state.get_parent()
-               
-    path.reverse()   
-    return path
+    def solve(self):
+        if self.method == 'bfs':
+            state = self.solve_bfs()
+
+        if self.method == 'dfs':
+            state = self.solve_dfs()
+
+        path = self.find_path(state)
+        self.running_time = time.time() - s
+        self.max_ram_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/(1024*1024)
+
+        f = open("output.txt","w")
+        f.write("path_to_goal: " + str(path) + "\n")
+        f.write("cost_of_path: " + str(len(path)) + "\n")
+        f.write("nodes_expanded: " + str(len(self.visited)-1) + "\n")
+        f.write("search_depth: " + str(state.depth) + "\n") # again
+        f.write("max_search_depth: " + str(self.max_search_depth) + "\n")
+        f.write("running_time: "+ "%.8f" %self.running_time + "\n") 
+        f.write("max_ram_usage: " + "%.8f" %self.max_ram_usage + "\n")
+        f.close() 
+
     
-l = [1,0,2,3,4,5,6,7,8]
+l = [1,2,5,3,4,0,6,7,8]
+g = solver(l,'dfs')
 
-print(solve_bfs(l), 'path_bfs')
-print(solve_dfs(l), 'path_dfs')
+g.solve()
+
         
             
             
